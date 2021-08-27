32f8250bca93ff2d544f3d91a4bc4e99012007d6
Danilo Nikolic <danilo@dramin-ns.com>
2021-08-27 23:32:52 +0200

    7 - 4:31

---
+++ EmployeeManagement.Web/Pages/EmployeeListBase.cs
@@ -0,0 +1,67 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class EmployeeListBase : ComponentBase
+    {
+        public IEnumerable<Employee> Employees { get; set; }
+
+        private void LoadEmployees()
+        {
+            Employee e1 = new Employee
+            {
+                EmployeeId = 1,
+                FirstName = "Pera",
+                LastName = "Peric",
+                Email = "pera@dramin-ns.com",
+                DateOfBirth = new DateTime(1980, 10, 5),
+                Gender = Gender.Male,
+                Department = new Department { DepartmentID = 1, DepartmentName = "nis" },
+                PhotoPath = "images/pera.png"
+            };
+
+            Employee e2 = new Employee
+            {
+                EmployeeId = 2,
+                FirstName = "Mika",
+                LastName = "Mikic",
+                Email = "mika@dramin-ns.com",
+                DateOfBirth = new DateTime(1975, 1, 15),
+                Gender = Gender.Male,
+                Department = new Department { DepartmentID = 1, DepartmentName = "nis" },
+                PhotoPath = "images/mika.png"
+            };
+
+            Employee e3 = new Employee
+            {
+                EmployeeId = 3,
+                FirstName = "Igor",
+                LastName = "Mudrinic",
+                Email = "igor@dramin-ns.com",
+                DateOfBirth = new DateTime(1975, 1, 15),
+                Gender = Gender.Male,
+                Department = new Department { DepartmentID = 1, DepartmentName = "nis" },
+                PhotoPath = "images/igor.png"
+            };
+
+            Employee e4 = new Employee
+            {
+                EmployeeId = 4,
+                FirstName = "Sara",
+                LastName = "Saric",
+                Email = "sara@dramin-ns.com",
+                DateOfBirth = new DateTime(1975, 1, 15),
+                Gender = Gender.Female,
+                Department = new Department { DepartmentID = 1, DepartmentName = "nis" },
+                PhotoPath = "images/sara.png"
+            };
+
+            Employees = new List<Employee> { e1, e2, e3, e4 };
+        }
+    }
+}
